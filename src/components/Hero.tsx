56d884b140b8dea2c3a425f4598c5634bf42e783
Victor Bravim <victorrafaelbravim@gmail.com>
2024-04-21 14:08:27 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,21 @@
 // Hero.tsx
 import Image from "next/image";
-import { FaPlay } from "react-icons/fa";
 
 export default function Hero() {
     return (
         <div id="home" className="block h-screen flex flex-col lg:flex-row items-center justify-center px-8 lg:px-16 lg:pt-0 pt-12">
             <div className="relative z-10 text-white w-full lg:w-1/3">
                 <div className="text-left mb-4">
-                    <p className="text-sm md:text-xl text-gray-custom font-bold mb-4 pr-0 lg:pr-20">TrueVR é a próxima evolução do mundo de realidade virtual. Nossa visão é ajudar a conectar todas as pessoas do mundo, eliminando distância e tempo. Comece sua aventura agora mesmo, juntos.</p>
+                    <h1 className="text-lg md:text-5xl mb-6"><strong>Sociallife</strong> - serviço de promoção em redes sociais</h1>
+                    <p className="text-sm md:text-xl text-gray-custom font-bold mb-4 pr-0 lg:pr-20">honest promotion</p>
                 </div>
                 <div className="flex flex-col lg:flex-row gap-6">
-                    <button className="bg-green-neon text-black text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded swv">
-                        Explorar
+                    <button className="bg-purple-custom text-white text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded-full swv">
+                    Experimente
                     </button>
                     <button className="bg-none hidden lg:block flex flex-row items-center text-white text-2xl font-bold py-3 px-10 rounded">
                         <div className="flex">
-                            <FaPlay className="mr-6" />
-                            Mais
+                            Inscreva-se
                         </div>
                     </button>
                 </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,8 @@
 'use client'
 import { useState } from 'react';
 import Image from "next/image";
+import logo from '@/assets/Logo.png'
+import { IoIosMenu } from "react-icons/io";
 
 export default function Nav() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -24,6 +26,7 @@ export default function Nav() {
         <nav className="bg-transparent p-8 absolute w-full z-20">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
                 <div className="flex items-center ml-0 lg:ml-8">
+                  <Image src={logo} alt="Logo" width={100} height={32} />
                 </div>
                 <div className="md:flex space-x-6 lg:space-x-12 mt-4 md:mt-0">
                     <button className="text-gray-400 text-base md:text-lg" onClick={() => scrollToSection('home')}>Home</button>
@@ -32,7 +35,7 @@ export default function Nav() {
                     <button className="text-gray-400 text-base md:text-lg" onClick={() => scrollToSection('recursos')}>Recursos</button>
                 </div>
                 <button className="bg-none hidden lg:block border border-green-custom text-green-custom rounded-md text-base md:text-lg font-bold py-2 px-4 mt-4 md:mt-0 mr-0 lg:mr-8">
-                    Sign up
+                  <IoIosMenu className='text-white'/>
                 </button>
             </div>
         </nav>
