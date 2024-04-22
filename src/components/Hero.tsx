7189347500ec23a02f6847f807da2bbabaeca124
Victor Bravim <victorrafaelbravim@gmail.com>
2024-04-22 20:47:14 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,11 +4,11 @@ import banner from '@/assets/banner.png'
 
 export default function Hero() {
     return (
-        <div id="home" className="block h-screen flex flex-col lg:flex-row items-center justify-center px-8 lg:px-16 lg:pt-0 pt-12 bg-cover bg-center" style={{backgroundImage: "url('/bg.png')"}}>
-            <div className="relative z-10 text-white w-full lg:w-1/3">
+        <div id="home" className="block h-screen flex flex-col lg:flex-row items-center justify-center px-8 lg:px-56 lg:pt-0 pt-12 bg-cover bg-center" style={{backgroundImage: "url('/bg.png')"}}>
+            <div className="relative z-10 text-white w-full lg:w-1/2">
                 <div className="text-left mb-4">
-                    <h1 className="text-lg md:text-5xl mb-6"><strong>Sociallife</strong> - serviço de promoção em redes sociais</h1>
-                    <p className="text-sm md:text-xl text-gray-custom font-bold mb-4 pr-0 lg:pr-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptates sunt possimus atque non excepturi maxime ex expedita tempora quaerat, animi et aspernatur blanditiis quas laboriosam praesentium iure corrupti id.</p>
+                    <h1 className="text-lg md:text-6xl mb-6"><strong>Sociallife</strong> - serviço de promoção em redes sociais</h1>
+                    <p className="text-sm md:text-lg text-gray-custom font-bold mb-4 pr-0 lg:pr-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptates sunt possimus atque non excepturi maxime ex expedita tempora quaerat, animi et aspernatur blanditiis quas laboriosam praesentium iure corrupti id.</p>
                 </div>
                 <div className="flex flex-col lg:flex-row gap-6">
                     <button className="bg-purple-custom text-white text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded-full swv">
