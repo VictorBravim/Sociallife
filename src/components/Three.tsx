463fe05c5b879fb00c18f18d5a32f4fdcf7c903a
Victor Bravim <victorrafaelbravim@gmail.com>
2024-04-22 16:44:00 -0300

    Mudança

---
+++ src/components/Three.tsx
@@ -0,0 +1,27 @@
+// Three.tsx
+import Image from "next/image";
+
+export default function Three() {
+    return (
+        <div id="home" className="block h-screen flex flex-col lg:flex-row items-center justify-center px-8 lg:px-16 lg:pt-0 pt-12 bg-cover bg-center" style={{backgroundImage: "url('/bg2.png')"}}>
+            <div className="relative z-10 text-white w-full lg:w-1/3">
+                <div className="text-left mb-4">
+                    <h1 className="text-lg md:text-5xl mb-6"><strong>Sociallife</strong> - serviço de promoção em redes sociais</h1>
+                    <p className="text-sm md:text-xl text-gray-custom font-bold mb-4 pr-0 lg:pr-20">honest promotion</p>
+                </div>
+                <div className="flex flex-col lg:flex-row gap-6">
+                    <button className="bg-purple-custom text-white text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded-full swv">
+                    Experimente
+                    </button>
+                    <button className="bg-none hidden lg:block flex flex-row items-center text-white text-2xl font-bold py-3 px-10 rounded">
+                        <div className="flex">
+                            Inscreva-se
+                        </div>
+                    </button>
+                </div>
+            </div>
+            <div className="w-full lg:w-1/2">
+            </div>
+        </div>
+    );
+}
