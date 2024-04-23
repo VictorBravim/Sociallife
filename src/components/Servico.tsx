0e41693445a0a0d0a1bf7fe83e5d0bec893e0923
Victor Bravim <victorrafaelbravim@gmail.com>
2024-04-23 19:45:23 -0300

    Mudança

--- src/components/Servico.tsx
+++ src/components/Servico.tsx
@@ -1,58 +1,59 @@
 // Servico.tsx
 
 export default function Servico() {
-    return (
-      <div
-        id="servico"
-        className="block h-screen flex flex-col items-center justify-center px-8 lg:px-56 lg:pt-0 pt-12 bg-cover bg-center"
-        style={{ backgroundImage: "url('/bg3.png')" }}
-      >
-        <div className="text-center text-white mb-16">
-          <p className="bg-pink-700 mx-24 rounded-full mb-2">Serviços</p>
-          <h1 className="text-lg md:text-2xl">Como nosso serviço funciona?</h1>
+  return (
+    <div
+      id="servico"
+      className="block h-screen flex flex-col items-center justify-center px-8 lg:px-56 lg:pt-0 pt-12 bg-cover bg-center"
+      style={{ backgroundImage: "url('/bg3.png')" }}>
+      <div className="text-center text-white mb-16">
+        <p className="bg-pink-700 mx-24 rounded-full mb-2">Serviços</p>
+        <h1 className="text-lg md:text-2xl">Como nosso serviço funciona?</h1>
+      </div>
+      <div className="relative gap-12 flex flex-row items-center justify-center z-10 text-white text-center w-full">
+        <div className="w-1/3">
+          <div className="bg-gray-custom p-8 rounded-lg">
+            <h1 className="text-xl font-bold text-white mb-4">SIMPLES</h1>
+            <h2 className="text-5xl font-bold text-purple-custom mb-4">R$24,99</h2>
+            <button className="border border-purple-custom p-2 px-10">Comprar</button>
+            <div className="border-b border-gray-600 mt-6 mb-4"></div>
+            <ul className="text-center text-xl text-gray-300">
+              <li className="p-2">✔ Acesso Limitado</li>
+              <li className="p-2">✔ 3 Tasks disponiveis</li>
+              <li className="p-2">✔ 4 Groups disponiveis</li>
+              <li className="p-2">✔ Suporte 24/7</li>
+            </ul>
+          </div>
         </div>
-        <div className="relative gap-12 flex flex-row items-center justify-center z-10 text-white text-center w-full">
-          <div className="w-1/3">
-            {/* Card Simples */}
-            <div className="bg-gray-custom p-8 rounded-lg">
-              <h2 className="text-4xl font-bold text-white mb-4">Simples</h2>
-              <ul className="text-center text-2xl text-gray-300">
-                <li className="p-2">Limited</li>
-                <li className="p-2">3 Tasks</li>
-                <li className="p-2">4 Groups</li>
-                <li className="p-2">24/7</li>
-                <li className="text-purple-custom">R$24,99</li>
-              </ul>
-            </div>
+        <div className="w-1/3">
+          <div className="bg-gray-custom p-8 rounded-lg">
+            <h1 className="text-xl font-bold text-white mb-4">BASICO</h1>
+            <h2 className="text-5xl font-bold text-purple-custom mb-4">R$54,99</h2>
+            <button className="border border-purple-custom p-2 px-10">Comprar</button>
+            <div className="border-b border-gray-600 mt-6 mb-4"></div>
+            <ul className="text-center text-xl text-gray-300">
+              <li className="p-2">✔ Acesso Ilimitado</li>
+              <li className="p-2">✔ 7 Tasks disponiveis</li>
+              <li className="p-2">✔ 10 Groups disponiveis</li>
+              <li className="p-2">✔ Suporte 24/7</li>
+            </ul>
           </div>
-          <div className="w-1/3">
-            {/* Card Básico */}
-            <div className="bg-gray-custom p-8 rounded-lg">
-              <h2 className="text-4xl font-bold text-white mb-4">Básico</h2>
-              <ul className="text-center text-2xl text-gray-300">
-                <li className="p-2">Unlimited</li>
-                <li className="p-2">5 Tasks</li>
-                <li className="p-2">10 Groups</li>
-                <li className="p-2">24/7</li>
-                <li className="text-purple-custom">R$54,99</li>
-              </ul>
-            </div>
-          </div>
-          <div className="w-1/3">
-            {/* Card Pro */}
-            <div className="bg-gray-custom p-8 rounded-lg">
-              <h2 className="text-4xl font-bold text-white mb-4">Pro+</h2>
-              <ul className="text-center text-2xl text-gray-300">
-                <li className="p-2">Unlimited</li>
-                <li className="p-2">10 Tasks</li>
-                <li className="p-2">15 Groups</li>
-                <li className="p-2">24/7</li>
-                <li className="text-purple-custom">R$84,99</li>
-              </ul>
-            </div>
+        </div>
+        <div className="w-1/3">
+          <div className="bg-gray-custom p-8 rounded-lg">
+            <h1 className="text-xl font-bold text-white mb-4">PRO+</h1>
+            <h2 className="text-5xl font-bold text-purple-custom mb-4">R$84,99</h2>
+            <button className="border border-purple-custom p-2 px-10">Comprar</button>
+            <div className="border-b border-gray-600 mt-6 mb-4"></div>
+            <ul className="text-center text-xl text-gray-300">
+              <li className="p-2">✔ Acesso Ilimitado</li>
+              <li className="p-2">✔ 10 Tasks disponiveis</li>
+              <li className="p-2">✔ 15 Groups disponiveis</li>
+              <li className="p-2">✔ Suporte 24/7</li>
+            </ul>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
