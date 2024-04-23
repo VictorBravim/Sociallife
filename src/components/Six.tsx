fdd2dfa64f86fbc6111dc5ec319a3d7028807fca
Victor Bravim <victorrafaelbravim@gmail.com>
2024-04-23 15:12:07 -0300

    Mudança

--- src/components/Six.tsx
+++ src/components/Six.tsx
@@ -9,20 +9,38 @@ export default function Six() {
                 <p className="bg-pink-700 mx-24 rounded-full mb-2">Work</p>
                 <h1 className="text-lg md:text-2xl">Como nosso serviço funciona?</h1>
             </div>
-            <div className="relative z-10 text-white w-full lg:w-1/2">
-                <div className="text-left mb-4">
-                    <h1 className="text-lg md:text-6xl mb-6"><strong>Sociallife</strong> - serviço de promoção em redes sociais</h1>
-                    <p className="text-sm md:text-lg text-gray-500 font-bold mb-4 pr-0 lg:pr-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptates sunt possimus atque non excepturi maxime ex expedita tempora quaerat, animi et aspernatur blanditiis quas laboriosam praesentium iure corrupti id.</p>
+            <div className="relative flex flex-row items-center justify-center gap-4 z-10 text-white text-center w-full">
+                <div className="text-left">
+                    <h2 className="p-2">Benefices</h2>
+                    <h2 className="bg-gray-custom p-2">Number of acount</h2>
+                    <h2 className="p-2">Number of tasks</h2>
+                    <h2 className="bg-gray-custom p-2">Connected groups</h2>
+                    <h2 className="p-2">Support</h2>
+                    <h2 className="bg-gray-custom p-2">Price</h2>
                 </div>
-                <div className="flex flex-col lg:flex-row gap-6">
-                    <button className="bg-purple-custom text-white text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded-full swv">
-                    Experimente
-                    </button>
-                    <button className="bg-none hidden lg:block flex flex-row items-center text-white text-2xl font-bold py-3 px-10 rounded">
-                        <div className="flex">
-                            Inscreva-se
-                        </div>
-                    </button>
+                <div className="text-center">
+                    <h2 className="p-2">Simple</h2>
+                    <h2 className="bg-gray-custom p-2">Limited</h2>
+                    <h2 className="p-2">3 Tasks</h2>
+                    <h2 className="bg-gray-custom p-2">Connected 4 groups</h2>
+                    <h2 className="p-2">24/7</h2>
+                    <h2 className="bg-gray-custom p-2">R$29.99</h2>
+                </div>
+                <div className="text-center">
+                    <h2 className="p-2">Basic</h2>
+                    <h2 className="bg-gray-custom p-2">Unlimited</h2>
+                    <h2 className="p-2">5 Tasks</h2>
+                    <h2 className="bg-gray-custom p-2">Connected 8 groups</h2>
+                    <h2 className="p-2">24/7</h2>
+                    <h2 className="bg-gray-custom p-2">R$49.00</h2>
+                </div>
+                <div className="text-center">
+                    <h2 className="p-2">Pro+</h2>
+                    <h2 className="bg-gray-custom p-2">Unlimited</h2>
+                    <h2 className="p-2">10 Tasks</h2>
+                    <h2 className="bg-gray-custom p-2">Connected 12 groups</h2>
+                    <h2 className="p-2">24/7</h2>
+                    <h2 className="bg-gray-custom p-2">R$69.99</h2>
                 </div>
             </div>
         </div>
